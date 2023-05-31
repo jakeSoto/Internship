e07de2caf7e67d88933b6f98992742def5105b3c
Jacob <jakesoto48@gmail.com>
2023-05-31 12:46:59 -0500

    init

---
+++ AI/driver.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+//#include "mac_mod.mac"
+//#include "keyPress.cpp"
+
+
+int main() {
+    openNIS();
+    //openImg();
+    //pressEnter();
+    //cleanup();
+
+    return 0;
+}
+
+openNIS() {
+    system("start C:\Program Files\NIS-Elements2022\nis_ar.exe");
+}
---
+++ AI/driver.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "testing.mac"
+using namespace std;
+
+int main() {
+	system("DATE");
+	
+	return 0;
+}
