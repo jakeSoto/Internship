3129ac2651587d6df56fc72645b6d302332a7e84
Jacob <jakesoto48@gmail.com>
2023-05-31 13:40:31 -0500

    testing

--- AI/driver.c
+++ AI/driver.c
@@ -1,16 +1,19 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 //#include "mac_mod.mac"
 //#include "keyPress.cpp"
 
 
 int main() {
-    // Open NIS application
-    system("start C:\\PROGRA~1\\NIS-Elements2022\\nis_ar.exe");
-
+    openNIS();
     //openImg();
     //pressEnter();
     //cleanup();
 
     return 0;
+}
+
+openNIS() {
+    system("start C:\\PROGRA~1\\NIS-Elements2022\\nis_ar.exe -m C:\\Users\\jakes\\OneDrive\\Documents\\GitHub\\Internship\\AI\\mac_mod.mac");
 }
