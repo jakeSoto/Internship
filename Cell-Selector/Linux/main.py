b646b49df9845041a0304d5ad0592152547c25ed
Jacob <jakesoto48@gmail.com>
2023-07-27 10:21:06 -0500

    linux version
    generalized variable names to adapt two time-series channels

---
+++ Cell-Selector/Linux/main.py
@@ -0,0 +1,52 @@
+import os, time
+import helper
+from openpyxl import Workbook
+
+
+def main():
+    start_time = time.time()
+    folderDict, dest = helper.searchDirectory()
+    imgRoot = os.path.join(dest, "Cells_")
+    traceRoot = os.path.join(dest, "Traces_")
+    dest = os.path.join(dest, "Cell_Data.xlsx")
+    channels = {}
+    wb = Workbook()
+
+    for i, folder in enumerate(folderDict):
+        if (folderDict[folder] == []):
+            continue
+
+        # Get container objects for channels in each folder
+        else:
+            channels = helper.createChannelDict(folderDict[folder])
+            sheet = wb.create_sheet(folder, i)
+
+        # 2 Channels
+        if (len(channels) < 3):
+            channels = helper.processTwoChannels(channels)
+
+        # 3 Channels
+        else:
+            channels = helper.processThreeChannels(channels)
+
+        # Export data
+        for i, channel in enumerate(channels):
+            print("Here")
+            imgName = imgRoot + str(channel) + ".png"
+            traceName = traceRoot + str(channel) + ".png"
+
+            dataSet = helper.transposeData(channels[channel].traces)
+            helper.saveCellImg(channels[channel].mask, imgName)
+            helper.saveTraces(dataSet, traceName)
+            helper.exportData(sheet, dataSet, ("Channel"+str(i+1)+" "), i+1)
+
+
+    wb.save(dest)
+    end_time = time.time()
+    total_time = (end_time - start_time) / 60
+    print("Done, results save to: " + str(dest))
+    print("Execution time: " + str(round(total_time, 1)) + " minutes")
+
+
+if __name__ == "__main__":
+    main()
