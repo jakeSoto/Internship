5c66660652db368383222ab0c31566e5a02c7c3c
Jacob <jakesoto48@gmail.com>
2023-08-09 17:18:39 -0500

    cellpose each frame of a stacked image
    export to a stacked tif image

--- Cell-Selector/Windows/AI_Segment/helper.py
+++ Cell-Selector/Windows/AI_Segment/helper.py
@@ -4,26 +4,33 @@ import numpy as np
 import tkinter as tk
 import matplotlib.pylab as plt
 from tkinter import filedialog
-from tkinter.filedialog import askdirectory
 from cellpose import models
 from multiprocessing import Pool
 import transients
 
 
 class container():
-  def __init__(self, fileName,
+    def __init__(self, fileName,
                     index = None,
+                    static = False,
                     raw = None,         # array of data
                     mask = None,        # array of cells locations
                     binaryMask = None,  # binary array of cells within the img
-                    traces = None       # array of cell value traces
+                    data = None,        # array of raw data from selected cells
+                    cellCount = None,   # number of cells
+                    traces = None,      # array of cell value traces
+                    region_cells = None # array of cell indices
                     ):
-    self.fileName = fileName
-    self.index = index
-    self.raw = raw
-    self.mask = mask
-    self.binaryMask = binaryMask
-    self.traces = traces
+        self.fileName = fileName
+        self.static = static
+        self.raw = raw
+        self.mask = mask
+        self.binaryMask = binaryMask
+        self.data = data
+        self.cellCount = cellCount
+        self.traces = traces
+        self.region_cells = region_cells
+
 
 class cellProp():
     def __init__(self, coords=None, area=None):
@@ -32,95 +39,54 @@ class cellProp():
 
 
 # Map folder names with file paths
-def searchDirectory() -> (dict, str):
+def getFile() -> (dict, str):
     window = tk.Tk()
     window.wm_attributes('-topmost', 1)
     window.withdraw()
-    root = askdirectory(title = "Select folder containing N_ folders...")
+    root = filedialog.askopenfile(mode='r', filetypes=[('Tiff Files', '*.tif')])
     if (not root):
         print("Operation cancelled")
         exit(1)
     else:
-        root = os.path.realpath(root)
-    
-    folderPaths = []
-    fileMap = {}
+        fileName = os.path.realpath(root.name)
 
-    for path, subdirs, files in os.walk(root):
-        temp = []
-        head, tail = os.path.split(path)
-
-        for file in files:
-            ext = os.path.splitext(file)[-1].lower()
-            if (ext == ".tif"):
-                filePath = os.path.realpath(os.path.join(path, file))
-                temp.append(filePath)
-            
-        fileMap[tail] = temp
-
-    return (fileMap, root)
-
-
-# Prompts user to select a Static Channel
-# Measures the values of cells from that channel
-def measuredChannel(folderDict) -> str:
-    print("Select the channel to measure")
-    window = tk.Tk()
-    window.wm_attributes('-topmost', 1)
-    window.withdraw()
-    targetFile = filedialog.askopenfile(title="Select the channel to measure...", filetypes=(('tif files','*.tif'), ('All files', '*,*')))
-    if (not targetFile):
-        print("Operation cancelled")
-        exit(1)
-    else:
-        targetPath = os.path.realpath(targetFile.name)
-
-    channelRef = None
-
-    for i, files in folderDict.items():
-        for file in files:
-            if (file == targetPath):
-                root, fileName = os.path.split(file)
-                channelName, ext = os.path.splitext(fileName)
-                channelRef = channelName
-
-    return channelRef
+    head, tail = os.path.split(fileName)
+    return (fileName, head)
 
 
 # Map channel name to container object
-def createChannelDict(fileNames: [str]) -> dict:
-    channelDict = {}
-    
-    for i, name in enumerate(fileNames):
-        root, file = os.path.split(name)
-        title, ext = os.path.splitext(file)
-        if (title == "mCherry"):
-            ar = transients.LoadTimeData(name, timeReversed = True)
-        else:
-            ar = transients.LoadStaticData(name, timeReversed = True)
+def createChannelContainer(fileName: str) -> container:
+    root, file = os.path.split(fileName)
+    title, ext = os.path.splitext(file)
+    print("File"+": "+file)
 
-        channelDict[title] = container(fileName=file, index=i, raw=ar)
+    try:
+        ar = transients.LoadTimeData(fileName, timeReversed = True)
+        static = False
+    except:
+        ar = transients.LoadStaticData(fileName, timeReversed = True)
+        static = True
 
-    return channelDict
+    channel = container(fileName=file, raw=ar, static=static)
+
+    return channel
 
 
 # Multi-procceses Cellpose with all channels
-def multiProcess(channelDict: dict) -> dict:
+def multiThread(channel: container) -> dict:
     dataSet = []
 
-    for channel in channelDict.values():
-        if (channel.fileName == "mCherry.tif"):
-            dataSet.append(channel.raw[0, :, :])
-        else:
-            dataSet.append(channel.raw)
+    if (channel.static == False):
+        for i in range(0, len(channel.raw)):
+            dataSet.append(channel.raw[i, :, :])
+    else:
+        dataSet.append(channel.raw)
 
-    with Pool() as pool:
+    with mp.Pool() as pool:
         results = pool.map(runCellpose, dataSet)
 
-    for channel in channelDict.values():
-        channel.mask = results[channel.index]
-
-    return channelDict
+    channel.mask = results
+    return channel
 
 
 # Calls Cellpose function
@@ -131,92 +97,25 @@ def runCellpose(data) -> []:
     return masks
 
 
-# Get cell indices within image
-def getRegionCells(mask, cellCount) -> []:
-    indices = []
-    areas = []
-
-    for i in range(1, cellCount+1):
-        index = np.argwhere(mask == i)
-        area = len(index)
-        indices.append(index)
-        areas.append(area)
-    
-    indices = np.array(indices, dtype=object)
-    areas = np.array(areas, dtype=object)
-
-    region_cells = []
-    for i in range(cellCount):
-        prop = cellProp(indices[i], areas[i])
-        region_cells.append(prop)
-
-    return region_cells
-
-
-# Normalization Formula = (x - min) / (max - min)
-def normalizeData(dataSet) -> []: 
-    normalized = [None] * len(dataSet)
-
-    for i in range(len(dataSet)):
-        trace = dataSet[i]
-        minVal = np.min(trace)
-        maxVal = np.max(trace)
-        norm = (trace - minVal) / (maxVal - minVal)
-        normalized[i] = norm
-
-    return normalized
-
-
-# Transposes data and exports to xlsx sheet
-def exportData(sheet, dataSet, title, count):
-    dataSet = np.array(dataSet)
-    dataSet = dataSet.T
-
-    for i, sublist in enumerate(dataSet):
-        for j, item in enumerate(sublist):
-            header = sheet.cell(row = 1, column = (j*4)+count)
-            header.value = (str(title) + str(j+1))
-
-            cell = sheet.cell(row = i+2, column = (j*4)+count)
-            cell.value = item
-
-
-# Save cells segmenation img
-def saveCellImg(mask, path):
-    plt.figure(figsize=(80,40))
-    ax1 = plt.subplot(131)
-    ax1.set_title('Selected Cells')
-    ax1.imshow(mask)
-    labels=[]
-
-    for i in range(mask.shape[0]):
-        for j in range(mask.shape[1]):
-            if (mask[i][j] == 0):
-                continue
-            else:
-                if (mask[i][j] not in labels):
-                    labels.append(mask[i][j])
-                    ax1.text(j, i, int(mask[i][j]), ha="center", va="center", fontsize=12, fontweight='black', color='orange')
-
-    plt.savefig(path, bbox_inches='tight')
-
-
-
-# Runs program on * channels
-def processChannels(channels: dict, dest: str) -> dict:
+# Runs program on two channels
+def processChannelData(channel: container, dest: str) -> dict:
     # Get masks
-    channels = multiProcess(channels)
+    channel = multiProcess(channel)
 
     # Process data
-    for key, channel in channels.items():
+    length = len(channel.mask)
+    print(length)
+    for i in range(length):
+        dataSet = channel.mask[i]
+
         # Binary mask
-        channel.binaryMask = np.zeros_like(channel.mask, int)
-        channel.binaryMask[channel.mask > 0] = 1
+        channel.binaryMask = np.zeros_like(dataSet, int)
+        channel.binaryMask[dataSet > 0] = 1
 
         # Raw data of selected cells
-        channel.data = channel.binaryMask * channel.raw
+        data = channel.binaryMask * channel.raw
 
-        # Export data to tiff file
-        tifffile.imwrite(dest, channel.data)
+        # Export
+        tifffile.imwrite(dest, data, metadata={'axes': 'TZYX', 'TimeIncrement': i/length})
 
-    return channels
+    return channel
